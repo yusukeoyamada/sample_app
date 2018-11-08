fc9e6dd97d9f8250103adb116241775ffd839bfb
yusukeoyamada <fa19940118@gmail.com>
2018-11-08 17:11:14 +0900

    タイトルヘルパーのテスト導入

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
   end
 end
